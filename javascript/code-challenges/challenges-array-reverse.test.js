b48cd65f717fc079d3b04a51f44d77605ef30d0c
Yazeed Aloufee <yazeedaloufee@gmail.com>
2022-02-06 08:47:58 +0200

    finished code challenge 1

--- javascript/code-challenges/challenges-array-reverse.test.js
+++ javascript/code-challenges/challenges-array-reverse.test.js
@@ -5,6 +5,17 @@
 
 //edge cases: 1) array is empty. 2) null or undefined. 
 //visual : start at the end of the array and copy the element in a new array and do the same from the end to the first element;
+
+//big O ?
+//time complexity O(n)
+// space complexity O(n)
+
+// Algorithm 
+//create a function that accepts an array
+//check if the array is empty, and return an empty array if it is
+// iterate the array starting with the last index
+// push the elemnt into an empty array.
+// return the new array 
 const arrayReverse=(array)=>{
   if(!array){return []}
   let testArray=[];
@@ -13,8 +24,14 @@ const arrayReverse=(array)=>{
   }
 return testArray;
 }
-
-
+// if want to make a copy of an array, you just use the "..." inside brackets
+const arr = [1,2,3,4]
+const reverse = (array)=>{
+  const arr2 = [...arr];
+  return  arr2.reverse();
+}
+console.log(reverse(arr))
+console.log(arr)
 
 console.log(arrayReverse([1,2,3]));
 console.log(arrayReverse([]));
